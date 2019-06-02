e4537bdcdf61446bfce0197c00c0da963e6c6309
t-sander85 <create@tomwsanders.com>
2019-06-02 04:44:02 +0000

    Added About

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,4 +7,23 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 
+
+  def create
+    @quote = Quote.create(quote_params)
+      if @quote.invalid?
+          flash[:error] = '<strong> How dare you!</strong> You have not entered a valid thing'
+        end
+    redirect_to root_path
+  end 
+
+  def about
+
+  end
+
+  private 
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end 
 end
+<br class="clear" />
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maximum: 140, minimum: 5 }
+  validates :author, presence: true, length: { maximum: 55, minimum: 5 }
 end
+ 
